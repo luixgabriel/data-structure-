a76d0c42d29bad5eb8500b91d71172936377db25
Luis Gabriel <luisgasbriel@gmail.com>
2023-10-15 20:14:48 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import java.util.Stack;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Stack<String> stack = new Stack<String>();
+        stack.push("One piece");
+        stack.push("Hajimme no Ippo");
+        System.out.println(stack);
+    }
+}
