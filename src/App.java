0526a4e600086ee35526f7e720b9ab367ee8490b
Luis Gabriel <luisgasbriel@gmail.com>
2023-10-17 15:00:44 -0300

    testing dynamic arrays

--- src/App.java
+++ src/App.java
@@ -1,11 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Stack;
 
 public class App {
     public static void main(String[] args) throws Exception {
         //testingStack();
-        testingQueue();
+        //testingQueue();
+        //testingPriorityQueue();
+        //testingLinkedList();
+        testingDynamicArray();
     }
 
     public static void testingStack() {
@@ -26,6 +33,7 @@ public class App {
     public static void testingQueue() {
          // FIFO DATA STRUCTURE
         Queue<String> queue = new LinkedList<String>();
+        //ENQUEUE
         queue.offer("Karen");
         queue.offer("Chad");
         queue.offer("Steve");
@@ -38,5 +46,44 @@ public class App {
         // System.out.println(queue.size());
         // System.out.println(queue.contains("Karen"));
         System.out.println(queue.peek());
+        System.out.println(queue);
+    }
+
+    public static void testingPriorityQueue(){
+        // FIFO DATA STRUCTURE
+        Queue<Double> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+        priorityQueue.offer(3.0);
+        priorityQueue.offer(6.0);
+        priorityQueue.offer(2.3);
+        priorityQueue.offer(11.10);
+
+        System.out.println(priorityQueue);
+    }
+
+    public static void testingLinkedList() {
+        LinkedList<String> linkedList = new LinkedList<String>();
+        linkedList.offer("A");
+        linkedList.offer("B");
+        linkedList.offer("C");
+        linkedList.offer("D");
+        linkedList.offer("F");
+        linkedList.indexOf(0);
+        linkedList.addFirst("wow");
+        linkedList.addLast("so awesome");
+        linkedList.removeFirst();
+        linkedList.removeLast();
+        System.out.println(linkedList);
+    }
+
+    public static void testingArrayList(){
+        ArrayList<String> arrayList = new ArrayList<String>();
+    }
+
+    public static  void testingDynamicArray(){
+
+        DynamicArray dynamicArray = new DynamicArray(12);
+        System.out.println(dynamicArray.capacity);
+        
     }
 }
